e0a74392ed34db2390a1ff35bf98b0ed218de384
Kunj Desai <kunjd41@gmail.com>
2025-08-05 20:13:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for API integration and calculations
+document.getElementById('activate-strategy').addEventListener('click', function() {
+    // Fetch data from Alpha Vantage API and perform calculations
+    console.log('Strategy activated!');
+});
